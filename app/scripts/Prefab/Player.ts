9ae3550eb1b9428bde82920fd2076b9e68bc8ffc
Rita Cheng <rita@ritacheng.com>
2014-10-29 10:03:03 -0700

    Merge branch 'origin/master'

--- app/scripts/Prefab/Player.ts
+++ app/scripts/Prefab/Player.ts
@@ -6,14 +6,33 @@ module Velcrelephant.Prefab
     {
       super(game, x, y, 'player', 0);
 
-      //loadTexture('sheet-princeProtect', 0, false);
-
       game.add.existing(this);
     }
 
     update()
     {
-      // Update prefab here
+      // keyboard logic
+      {
+        var keyboard:Phaser.Keyboard = this.game.input.keyboard;
+
+        if (keyboard.isDown(Phaser.Keyboard.RIGHT))
+        {
+          this.x += 2;
+        }
+        else if (keyboard.isDown(Phaser.Keyboard.LEFT))
+        {
+          this.x -= 2;
+        }
+
+        if (keyboard.isDown(Phaser.Keyboard.DOWN))
+        {
+          this.y += 2;
+        }
+        else if (keyboard.isDown(Phaser.Keyboard.UP))
+        {
+          this.y -= 2;
+        }
+      }
     }
   }
 }
